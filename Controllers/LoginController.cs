8cc4bb63684adea5691d9af076e7d333ff2c3b40
langoclinh2312 <vnbboy12345@gmail.com>
2023-07-02 23:17:07 +0700

    code main

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using sem3.Areas.Admin.Models;
+using sem3.Data;
+using sem3.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace sem3.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+		public LoginController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Login(Login model)
+		{
+			if (ModelState.IsValid)
+			{
+				User acc = _context.Users.FirstOrDefault(a => a.Email.Equals(model.Email) && a.Password.Equals(model.Password));
+
+				if (acc != null)
+				{
+					HttpContext.Session.SetString("AppLogin", JsonConvert.SerializeObject(acc));
+					return RedirectToAction("Index", "Home");
+				}
+				else
+				{
+					ModelState.AddModelError("Email", "Tài khoản hoặc mật khẩu không chính xác");
+				}
+			}
+			return View(model);
+		}
+		public IActionResult Register()
+		{
+
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Register(User user)
+		{
+			if (ModelState.IsValid)
+			{
+				var check = _context.Users.FirstOrDefault(a => a.Email == user.Email);
+				if (check == null)
+				{
+					user.Password = GetMD5(user.Password);
+					_context.ChangeTracker.LazyLoadingEnabled = false;
+					_context.Users.Add(user);
+					_context.SaveChanges();
+					return RedirectToAction("Index", "Home");
+				}
+				else
+				{
+					ViewBag.error = "Email already exists";
+					return View();
+				}
+
+
+			}
+			return View();
+
+		}
+		private string GetMD5(string password)
+		{
+			MD5 md5 = new MD5CryptoServiceProvider();
+			byte[] fromData = Encoding.UTF8.GetBytes(password);
+			byte[] targetData = md5.ComputeHash(fromData);
+			string byte2String = null;
+
+			for (int i = 0; i < targetData.Length; i++)
+			{
+				byte2String += targetData[i].ToString("x2");
+
+			}
+			return byte2String;
+		}
+		public IActionResult Logout()
+		{
+			HttpContext.Session.Remove("AdminLogin");
+			return RedirectToAction("Login");
+		}
+	}
+}
---
+++ Response/HomeResponse.cs
@@ -0,0 +1,8 @@
+﻿using sem3.Models;
+
+namespace sem3.Response;
+
+public class HomeResponse
+{
+    public List<Blog> Blogs { get; set; }
+}
